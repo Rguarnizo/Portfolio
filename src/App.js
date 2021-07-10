0e378a3057c307d422ce4bf551c4214c24605fe3
rguarnizo <rguarnizo@unal.edu.co>
2021-07-09 22:59:50 -0500

    Add Japanese Lamps to scene

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 import Sakura from "./3DComponents/sakura";
-
+import Lamp from "./3DComponents/lamp"
 
 function LowPolyTokyo() {
   const tokyo = useLoader(GLTFLoader, "Models/LowpolyTokyo/Test/Test/LowPolyTokyo.gltf");
@@ -86,6 +86,11 @@ function LowPolyTokyo() {
         <Sakura position={[-4,0,-5]}  rotation={[0,0,0]}/>
         <Sakura position={[-12,0,-8]}  rotation={[0,Math.PI/2,0]}/>
         <Sakura position={[10,0,-11]} rotation={[0,Math.PI/2,0]}/>
+        <Lamp   position={[-4,7,-4]}/>
+        <Lamp   position={[-4,9,-2]}/>
+        <Lamp   position={[-5,8,-6]}/>
+        <Lamp   position={[-0.7,13,9.5]} intensity={1.5}/>
+        <Lamp   position={[2.9,13,9.5]} intensity={1.5}/>
         
     </>
   );
@@ -99,11 +104,12 @@ function App() {
   return (
     <>
       <Canvas camera={{ position: [-50, 0, 10] }} id={"scene"} shadows>
-        <ambientLight intensity={0.3}/>
+        {/* <ambientLight intensity={0.3}/> */}
         {/* <OrbitControls/> */}
         <Suspense fallback={null}>
           <LowPolyTokyo />
         </Suspense>
+        
         <axesHelper />
       </Canvas>
       <div className="main">
