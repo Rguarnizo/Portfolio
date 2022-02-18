bb2362a4e626956b8cfac98aa006001253133b32
rguarnizo <rguarnizo@unal.edu.co>
2022-02-18 18:59:36 -0500

    Add font magilio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-[#fac9c3]">
-    
+    <div className=" text-[10vw] h-screen w-screen text-black font-magilio bg-[#fac9c3]">
+      <div className="h-full w-full mx-auto flex flex-row justify-center items-center">
+        <p className="up">Ruben</p> 
+        <p className="down">Dario</p>
+      </div>
     </div>
   );
 }
